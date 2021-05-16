3ad593f1aabdc0a41b031a4d44ad36a59615bc9b
Longjun Song <logensong@gmail.com>
2021-05-17 00:34:28 +0800

    implement part of the methods

--- WordNet/src/Outcast.java
+++ WordNet/src/Outcast.java
@@ -2,15 +2,14 @@ public class Outcast {
 
     // constructor takes a WordNet object
     public Outcast(WordNet wordnet) {
-
     }
 
     // given an array of WordNet nouns, return an outcast
     public String outcast(String[] nouns) {
-
+        return null;
     }
 
     public static void main(String[] args) {
-
+        // unit test code
     }
 }
--- WordNet/src/WordNet.java
+++ WordNet/src/WordNet.java
@@ -1,15 +1,54 @@
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.ST;
+
 public class WordNet {
+    private final ST<String, Integer> dict;
+    private final Digraph wordGraph;
+    private int cntOfNodes = 0;
+
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
         if (synsets == null || hypernyms == null) {
             throw new IllegalArgumentException("" +
                     "Arguments for constructor can not be null");
         }
+
+        dict = new ST<>();
+        In in = new In(synsets);
+        while (in.hasNextLine()) {
+            String line = in.readLine();
+            // separate id, synsets, definition
+            String[] items = line.split(",");
+            // separate synonyms
+            String[] synonyms = items[1].split(" ");
+
+            int nodeId = Integer.parseInt(items[0]);
+            for (var syn : synonyms) {
+                dict.put(syn, nodeId);
+            }
+            ++cntOfNodes;
+        }
+        in.close();
+
+        wordGraph = new Digraph(cntOfNodes);
+
+        in = new In(hypernyms);
+        while(in.hasNextLine()) {
+            String line = in.readLine();
+            String[] items = line.split(",");
+            int v = Integer.parseInt(items[0]);
+            for (int ix = 1; ix < items.length; ++ix) {
+                wordGraph.addEdge(v, Integer.parseInt(items[ix]));
+            }
+        }
+
+        //TODO(ljsong): handle the exception that word net is not a rooted DAG
     }
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
-        return null;
+        return dict.keys();
     }
 
     // is the word a WordNet noun?
@@ -19,7 +58,7 @@ public class WordNet {
                     "Arguments for isNoun method can not be null");
         }
 
-        return false;
+        return dict.contains(word);
     }
 
     // distance between nounA and nounB (defined below)
